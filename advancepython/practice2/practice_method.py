6c066bd985d6eb5ecd995697374c1bc0209b0e25
IrfanMulani01 <irfanmulani0931@gmail.com>
2026-03-24 17:00:47 +0530

    add one question and solved it

---
+++ advancepython/practice2/practice_method.py
@@ -0,0 +1,15 @@
+## Q1. Create a class Student with instance variables name and marks. Add an instance method display() that prints both. 
+# Create 2 objects with different data and call display() on each.
+# class Student:  # create class name Student
+#     def __init__(self, name , marks):   # create cunstructor
+#         self.name = name    ##
+#         self.marks = marks  # create instance variable
+#     def display(self):  # create instance method
+#         print(f"Student name is {self.name}")   # print both name and marks
+#         print(f"Student Marks is {self.marks}")
+#         return
+# s1 = Student("irfan", 123)  #create first object
+# s1.display()    
+
+# s2 = Student("ritesh", 234) # create second object
+# s2.display()
