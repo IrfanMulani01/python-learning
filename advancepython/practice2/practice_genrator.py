54e11116c1b114d19a6285dd3c545ea0028c3dd7
IrfanMulnai01 <irfanmulani0931@gmail.com>
2026-03-26 13:53:09 +0530

    solving questions

---
+++ advancepython/practice2/practice_genrator.py
@@ -0,0 +1,17 @@
+## Q1. Write a simple generator function cu(n) that yields numbers from 1 to n one by one. Use a for loop to print all values.
+# def count_up():
+#     for i in range(1,10):
+#         yield i
+# cu = count_up()
+
+# print(next(cu))
+# print(next(cu))
+# print(next(cu))
+# print(next(cu))
+# print(next(cu))
+# print(next(cu))
+# print(next(cu))
+# print(next(cu))
+# print(next(cu))
+# print(next(cu))
+# print(next(cu))
