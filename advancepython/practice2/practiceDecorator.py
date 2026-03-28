8eb153ae44264d91af337c42074404b4b7848693
IrfanMulani01 <irfanmulani0931@gmail.com>
2026-03-28 12:29:45 +0530

    solving questions in practiceDecorator.py file and add contextManger.py file

--- advancepython/practice2/practiceDecorator.py
+++ advancepython/practice2/practiceDecorator.py
@@ -203,3 +203,200 @@
 # ac = accessControl("irfan")
 # ac.download_file()
 
+
+## Write a decorator @greet that prints "Starting..." before and "Done!" after any function runs.
+# def greet(func):
+#     def wrapper():
+#         print("Starting................")
+#         func()
+#         print("....................End")
+#     return wrapper
+
+# @greet
+# def show():
+#     print("c=a+b")
+# show()
+
+## Create a @uppercase decorator that converts a function's string return value to uppercase.
+# def uppercase(func):
+#     def wrapper(*args, **kwargs):
+#         result = func(*args, **kwargs)
+#         return result.upper()
+#     return wrapper
+
+# @uppercase
+# def show():
+#     return "irfan"
+# print(show())
+
+## Write a @repeat(n) decorator that runs a function n times.
+# def repeat(n):
+#     def decorator(func):
+#         def wrapper(*args):
+#             result = func(*args)
+#             res = result * n
+#             return res
+#         return wrapper
+#     return decorator
+
+# @repeat(3)
+# def show():
+#     return "hii\n"
+# print(show())
+
+## Build a @timer decorator that prints how long a function took to execute.
+import time
+# def timer(func):
+#     def wrapper(*args, **kwargs):
+#         start = time.time()
+#         result = func(*args, **kwargs)
+#         end = time.time()
+#         print(f"{func.__name__} took {end - start: .4f} s")
+#         return result 
+#     return wrapper
+
+# @timer
+# def show():
+#     return time.sleep(6)
+# print(show())
+
+## Write a @validate_args decorator that checks all arguments to a function are positive integers — raise ValueError if not.
+# def validate_args(func):
+#     def wrapper(args):
+#         result = func(*args)
+#         try:
+#             if args > 0:
+#                 print("Enter positive number")
+#             else:
+#                 print(f"you enter positive number")
+#             return
+#         except ValueError:
+#             raise ValueError
+#         return result
+#     return wrapper
+
+# @validate_args
+# def show(args):
+#     raise ValueError
+# print(show(2))
+
+## Write a @requires_role(role) decorator for a user system that blocks access if the user's role doesn't match.
+# def requires_role(role):
+#     def decorator(func):
+#         def wrapper(args):
+#             result = func(args)
+#             try:
+#                 if role == args:
+#                     print("Access Granted")
+#                 else:
+#                     print("access deny")
+                    
+#             except ValueError:
+#                 print("enter valid role")
+#             return result
+#         return wrapper
+#     return decorator
+
+# @requires_role("coder")
+# def show(args):
+#     raise ValueError
+# show("coder")
+
+## Write a class-based decorator @Singleton that ensures only one instance of a class is ever created.
+# class single_ton:
+#     def __init__(self, cls):
+#         self._cls = cls
+#         self._instance = None
+
+#     def __call__(self, *args, **kwargs):
+#         if self._instance is None:
+#             self._instance = self._cls(*args, **kwargs)
+#         return self._instance
+    
+# @single_ton
+# class databaseconnection:
+#     def __init__(self, url):
+#         self.url = url
+#         print(f"database url is: {url}")
+
+# st = databaseconnection(
+#     "localhost: 8080"
+# )
+# st1 = databaseconnection(
+#     "localhost: 9080"
+# )
+# st2 = databaseconnection(
+#     "localhost: 3080"
+# )
+
+## Write a generator function count_up(n) that yields numbers from 1 to n one at a time.
+# def count_up(n):
+#     for i in range(n+1):
+#         yield i
+    
+# print(sum(count_up(5)))
+
+
+##  Create a generator that yields only even numbers from a given list.
+
+# def even():
+#     lst = [1,2,3,4,5,6,7,8,9,11,12,13,14,151,55,16]
+#     for i in lst:
+#         if i % 2 == 0:
+#             yield i
+
+# e = even()
+# print(next(e))
+
+# while True:
+#     try:
+#         print(next(e))
+#     except StopIteration:
+#         print("no more iteration")
+#         break    
+    
+## Use next() manually on a generator and handle StopIteration gracefully.
+# def even():
+#     lst = [1,2,3,4,5,6,7,8,9,11,12,13,14,151,55,16]
+#     for i in lst:
+#         if i % 2 == 0:
+#             yield i
+# e = even()
+
+# while True:
+#     try:
+#         value = next(e)
+#         print(value)
+#     except StopIteration:
+#         print("no more items in list")
+#         break
+
+
+## Write a generator fibonacci() that yields an infinite Fibonacci sequence — stop it with a for loop and break.
+# def fib(n):
+#     a, b=0,1
+#     while True:
+#         yield a
+#         a, b= b,a+b
+        
+# f = fib(5)
+# # while True:
+# #     try:
+# #         print(next(f))
+        
+# #     except:
+# #         print("stop itertion")
+# #         break   
+
+
+## Build a chunked(lst, size) generator that yields a list in chunks of given size.
+def chuncked(lst,size):
+    if size < 0:
+        raise ValueError("you must enter positive number")
+    for i in range(0,len(lst), size):
+        yield lst[i:i + size]
+
+data = [1,12,2,43,4546,54,654,7,547,546,724,4,2,43,534,63,6,36,52,34,53]
+
+for i in chuncked(data, 8):
+    print(i)
